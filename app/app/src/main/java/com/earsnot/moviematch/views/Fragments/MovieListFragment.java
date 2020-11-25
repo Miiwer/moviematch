94065a228b7cb860e2000a7f7bef2a15da746932
Mie Nielsen <mie.240892@gmail.com>
2020-11-25 18:26:42 +0100

    Cleaned up MovieListFragment

--- app/app/src/main/java/com/earsnot/moviematch/views/Fragments/MovieListFragment.java
+++ app/app/src/main/java/com/earsnot/moviematch/views/Fragments/MovieListFragment.java
@@ -16,46 +16,12 @@ import com.earsnot.moviematch.views.LoginActivity;
 import com.earsnot.moviematch.views.MainActivity;
 import com.firebase.ui.auth.AuthUI;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MovieListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MovieListFragment extends Fragment {
 
     // Just testing - Mie
     private Button mBtnLogout;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MovieListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MovieListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MovieListFragment newInstance(String param1, String param2) {
-        MovieListFragment fragment = new MovieListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -75,20 +41,9 @@ public class MovieListFragment extends Fragment {
             }
 
         });
-
-
         return view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-
-        }
-    }
 
     private void logout() {
         AuthUI.getInstance()
@@ -104,5 +59,4 @@ public class MovieListFragment extends Fragment {
         startActivity(i);
         getActivity().finish();
     }
-
 }
